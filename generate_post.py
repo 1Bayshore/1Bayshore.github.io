346df20f31c4c6978a01658176d4388fd4c19291
1Bayshore <owen@luebke.us>
2024-05-10 12:25:14 -0500

    generate_post now uses BeautifulSoup! Added an example post with an example tag to practise deleting, adding, and modifying posts Updated the style of the templates slightly

--- generate_post.py
+++ generate_post.py
@@ -1,146 +1,314 @@
+from bs4 import BeautifulSoup
 import datetime
 from pathlib import Path
 
 # First, take input from the user
-print("Please enter the filename, as listed in the 'sources' directory:")
-filename = input("filename: ")
-if not filename.endswith(".txt"):
-    filename += ".txt"
 
-print("Please enter a title for the post:")
-title = input("title: ")
-if title == "":
-    title = "Untitled"
+print("Select a mode: (n)ew, (u)pdate, or (d)elete:")
+mode_txt = input("mode: ")
+while mode_txt not in ["n", "new", "u", "update", "d", "delete"]:
+    print("Please enter a valid mode:")
+    mode_txt = input("mode: ")
 
-print("Please input any tags for the post, separated by commas:")
-print("(Note: tags should be valid in urls)")
-tags_str = input("tags: ")
+mode = ""
 
-# Next, read the source file and the template
-text = ""
-with open("sources/" + filename) as f:
-    text = f.read()
+if mode_txt in ["n", "new"]:
+    mode = "new"
 
-template = ""
-with open("template.html") as f:
-    template = f.read()
+elif mode_txt in ["u", "update"]:
+    mode = "update"
+
+elif mode_txt in ["d", "delete"]:
+    mode = "delete"
+
+if mode == "new":
+    print("Please enter the filename of the source text, as listed in the 'sources' directory:")
+    filename = input("filename: ")
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+    filename = filename.rpartition("/")[2]
+
+    print("Please enter a title for the post:")
+    title = input("title: ")
+    if title == "":
+        title = "Untitled"
+
+    print("Please input any tags for the post, separated by commas:")
+    print("(Note: tags should be valid in urls)")
+    tags_str = input("tags: ")
+
+elif mode == "update":
+    print("Please enter the url of the post, including everything after the first slash:")
+    url = input("url: ")
+    if not url.startswith("/"):
+        url = "/" + url
+    
+    filename = url.rpartition("/")[2].replace(".html", ".txt")
+
+    print("Please enter the new title for the post:")
+    title = input("title: ")
+    if title == "":
+        title = "Untitled"
+
+    print("Please input any tags for the post, separated by commas:")
+    print("(Note: tags should be valid in urls)")
+    print("(Note: replaces all existing tags)")
+    tags_str = input("tags: ")
+
+elif mode == "delete":
+    print("Please enter the url of the post, including everything after the first slash:")
+    url = input("url: ")
+    if not url.startswith("/"):
+        url = "/" + url
+
+#  Next, read the source file and the template
+if mode != "delete":
+    text = ""
+    with open("sources/" + filename) as f:
+        text = f.read()
+
+    template = ""
+    with open("template.html") as f:
+        template = f.read()
 
 # Customise the template text with the text from the source
 
-text = text.replace("\t", "<p>") # note actual tab character
-text = text.replace("\n", "</p>\n")
-if not text.endswith("</p>"):
-    text += "</p>"
+    soup = BeautifulSoup(template, 'html.parser')
+
+    soup.title.insert(0, title)
+    soup.find("h1", class_="title").append(title)
+
+    for i in text.splitlines():
+        t_line = i.strip()
+        soup.find("div", class_="body").append(BeautifulSoup("<p>" + t_line + "</p>", "html.parser"))
 
 # Separate the tags by commas to make a list, and turn that into <a> tags
 
-tags_list = [x.strip() for x in tags_str.split(",")]
-tags_html = "Tags: "
-for i in tags_list:
-    tags_html += f'<a href="/tags/{i}.html">{i}</a>, '
+    tags_list = [x.strip() for x in tags_str.split(",")]
+    tags_html = ""
+    for i in tags_list:
+        tags_html += f'<a href="/tags/{i}.html">{i}</a>, '
 
-tags_html = tags_html[:-2] # remove the last comma and space
+    tags_html = tags_html[:-2] # remove the last comma and space
+    soup.find("div", class_="tags").append(BeautifulSoup(tags_html, "html.parser"))
+
 
 # Make sure the filename of the output is valid for a url (TODO: More checking here)
 
-output_filename = filename.partition(".")[0]
-output_filename = output_filename.replace(" ", "-")
-output_filename = output_filename.replace("_", "-")
+    output_filename = filename.partition(".")[0]
+    output_filename = output_filename.replace(" ", "-")
+    output_filename = output_filename.replace("_", "-")
 
 # Get the current date, to use as an index for the post
-current_time = datetime.date.today()
+if mode == "new":
+    current_time = datetime.date.today()
 
 # Create the directory in which the file will go
 
-dir_path = Path(f"site/{current_time.year}/{current_time.month}/{current_time.day}")
-dir_path.mkdir(parents=True, exist_ok=True)
+    dir_path = Path(f"site/{current_time.year}/{current_time.month}/{current_time.day}")
+    dir_path.mkdir(parents=True, exist_ok=True)
 
 # Get the previous post from the previous post file
 
-prev_post_link = ""
+    prev_post_link = ""
 
-try:
-    with open("prev_post_link.txt") as f:
-        prev_post_link = f.read()
-except FileNotFoundError:
-    pass # just leave the string blank
+    try:
+        with open("prev_post_link.txt") as f:
+            prev_post_link = f.read()
+    except FileNotFoundError:
+        pass # just leave the string blank
 
-# Finalise the template
+    if prev_post_link:
+        soup.find("a", class_="prev")['href'] = prev_post_link
+    
+elif mode == "update":
+    next_link = ""
+    prev_link = ""
+    with open("site" + url) as f:
+        n_text = f.read()
+        soup2 = BeautifulSoup(n_text, "html.parser")
+        try:
+            prev_link = soup2.find("a", class_="prev")['href']
+        except KeyError:
+            pass # modifying the first post
+        try:
+            next_link = soup2.find("a", class_="next")['href']
+        except KeyError:
+            pass # modifying the last post
+    if prev_link:
+        soup.find("a", class_="prev")['href'] = prev_link
+    if next_link:
+        soup.find("a", class_="next")['href'] = next_link
 
-final_html = template.replace("{{title}}", title)
-final_html = final_html.replace("{{body}}", text)
-final_html = final_html.replace("{{tags}}", tags_html)
+# Save tags for later (if updating)
 
-if prev_post_link: # account for first post, where there is no prev_post_link
-    final_html = final_html.replace('<a class="prev">Previous</a>', f'<a class="prev" href="{prev_post_link}">Previous</a>')
+if mode == "update":
+    d_tags_list = []
+    with open("site" + url) as f:
+        f_content = f.read()
+        soup3 = BeautifulSoup(f_content, "html.parser")
+        for i in soup3.find("div", class_="tags").find_all("a"):
+            d_tags_list.append(i.text.strip())
 
-# Write the file. Will raise an error if the file already exists
+# If in "new" mode, write the file. Will raise an error if the file already exists
 
-with open(f"site/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html", "x") as f:
-    f.write(final_html)
+if mode == "new":
+    with open(f"site/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html", "x") as f:
+        f.write(soup.prettify())
+
+# If in "update" mode, replace the file, first confirming that it already exists
+elif mode == "update":
+    with open("site" + url, "w") as f:
+        f.write(soup.prettify())
+
+# If in "delete" mode, delete the file, first saving the tags (to be deleted later) and the next link of the previous page
+elif mode == "delete":
+    prev_page = ""
+    d_tags_list = []
+    with open("site" + url) as f:
+        f_content = f.read()
+        soup3 = BeautifulSoup(f_content, "html.parser")
+        prev_page = soup3.find("a", class_="prev").string
+        for i in soup3.find("div", class_="tags").find_all("a"):
+            d_tags_list.append(i.text.strip())
+    
+    del_path = Path("site" + url)
+    del_path.unlink()
 
 # Set the previous post file to this post
 
-with open("prev_post_link.txt", "w") as f:
-    f.write(f"/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html")
+if mode == "new":
+    with open("prev_post_link.txt", "w") as f:
+        f.write(f"/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html")
 
-# Add a link to the file on the homepage (index.html)
+# Add a link to the file on the homepage (index.html) if necessary
 
-link_txt = f'<p><b>{current_time.year}-{current_time.month}-{current_time.day}: </b><a href="/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html">{title}</a></p>\n'
+if mode == "new":
+    link_txt = f'<p><b>{current_time.year}-{current_time.month}-{current_time.day}: </b><a href="/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html">{title}</a></p>\n'
 
-idx_page = ""
+    idx_page = ""
 
-with open("site/index.html") as f:
-    idx_page = f.read()
+    with open("site/index.html") as f:
+        idx_page = f.read()
 
-idx_page = idx_page.replace("<!--Most recent post marker - do not remove-->", "<!--Most recent post marker - do not remove-->\n" + link_txt)
+    soup4 = BeautifulSoup(idx_page, "html.parser")
+    soup4.find("div", class_="recentPosts").insert(0, BeautifulSoup(link_txt, "html.parser"))
 
-with open("site/index.html", "w") as f:
-    f.write(idx_page)
+    with open("site/index.html", "w") as f:
+        f.write(soup4.prettify())
+
+if mode == "update":
+    link_txt = f'<p><b>{url.split("/")[1]}-{url.split("/")[2]}-{url.split("/")[3]}: </b><a href="{url}">{title}</a></p>\n'
+
+# Delete link to the file on the homepage (index.html) if necessary
+
+elif mode == "delete":
+    idx_page = ""
+    with open("site/index.html") as f:
+        idx_page = f.read()
+    
+    soup5 = BeautifulSoup(idx_page, "html.parser")
+    soup5.find("div", class_="recentPosts").find("p").replace_with("")
+
+    with open("site/index.html", "w") as f:
+        f.write(soup5.prettify())
 
 # Create and/or update the tag pages as necessary
 
-for i in tags_list:
-    try:
-        tag_page_content = ""
+if mode in ["update", "delete"]:
+    for i in d_tags_list:
+        print(d_tags_list)
+        try:
+            tag_page_content = ""
+            with open(f"site/tags/{i}.html") as f:
+                tag_page_content = f.read()
+            
+            soup6 = BeautifulSoup(tag_page_content, "html.parser")
+            for j in soup6.find("div", class_="body").find_all("p"):
+                if i in j:
+                    j.replace_with("")
 
-        with open(f"site/tags/{i}.html") as f:
-            tag_page_content = f.read()
-        tag_page_content = tag_page_content.replace("<!--Most recent post marker - do not remove-->", "<!--Most recent post marker - do not remove-->\n" + link_txt)
+            with open(f"site/tags/{i}.html", "w") as f:
+                f.write(soup6.prettify())
 
-        with open(f"site/tags/{i}.html", "w") as f:
-            f.write(tag_page_content)
-    
-    except FileNotFoundError:
-        tag_page_content = ""
+        except FileNotFoundError:
+            pass
 
-        with open("tag_template.html") as f:
-            tag_page_content = f.read()
-        
-        tag_page_content = tag_page_content.replace("{{tag}}", i)
-        tag_page_content = tag_page_content.replace("{{body}}", link_txt)
+if mode in ["new", "update"]:
+    for i in tags_list:
+        try:
+            tag_page_content = ""
+            with open(f"site/tags/{i}.html") as f:
+                tag_page_content = f.read()
+            
+            soup7 = BeautifulSoup(tag_page_content, "html.parser")
+            soup7.find("div", class_="body").insert(0, BeautifulSoup(link_txt, "html.parser"))
 
-        with open(f"site/tags/{i}.html", "w") as f:
-            f.write(tag_page_content)
+            with open(f"site/tags/{i}.html", "w") as f:
+                f.write(soup7.prettify())
+
+        except FileNotFoundError:
+            tag_page_content = ""
+            with open("tag_template.html") as f:
+                tag_page_content = f.read()
+            
+            soup8 = BeautifulSoup(tag_page_content, "html.parser")
+            soup8.title = i
+            soup8.find("h1", class_="title").insert(0, i)
+            soup8.find("div", class_="body").insert(0, BeautifulSoup(link_txt, "html.parser"))
+
+            with open(f"site/tags/{i}.html", "w") as f:
+                f.write(soup8.prettify())
 
 # Update the "next" link on the previous post to point to this one
 
-prev_post_html = ""
+if mode == "new":
+    prev_post_html = ""
 
-if prev_post_link:
+    if prev_post_link:
+        try:
+            with open("site" + prev_post_link) as f:
+                prev_post_html = f.read()
+                        
+            soup9 = BeautifulSoup(prev_post_html, "html.parser")
+            tag = soup9.find("a", "next")
+            tag['href'] = f"/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html"
+
+            with open("site" + prev_post_link, "w") as f:
+                f.write(soup9.prettify())
+
+        except FileNotFoundError or PermissionError:
+            pass # it's the first post, no previous one to be updated
+
+# If deleting the most recent post, reset the "next" pointer to the previous one
+
+if mode == "delete":
+    prev_post_link = ""
+
     try:
-        with open("site" + prev_post_link) as f:
-            prev_post_html = f.read()
-        
-        prev_post_html = prev_post_html.replace('<a class="next">Next</a>', f'<a class="next" href="/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html">Next</a>')
+        with open("prev_post_link.txt") as f:
+            prev_post_link = f.read()
+    except FileNotFoundError:
+        pass # just leave the string blank
 
-        with open("site" + prev_post_link, "w") as f:
-            f.write(prev_post_html)
+    if prev_post_link == url:
+        prev_site = ""
+        with open("site" + prev_page) as f:
+            prev_site = f.read()
+        soup10 = BeautifulSoup(prev_site, "html.parser")
+        soup10.find("a", class_="next")['href'] = ""
 
-    except FileNotFoundError or PermissionError:
-        pass # it's the first post, no previous one to be updated
+        with open("site" + prev_page, "w") as f:
+            f.write(soup10.prettify())
 
+        with open("prev_post_link.txt", "w") as f:
+            f.write(prev_page)
 
 # Report success
 
-print("Post generated!")
+if mode == "new":
+    print("Post generated!")
+elif mode == "update":
+    print("Post updated!")
+elif mode == "delete":
+    print("Post deleted.")
