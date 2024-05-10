62a53eb41fab1358349ab5d9c83c04dc4b32d916
1Bayshore <owen@luebke.us>
2024-05-09 19:20:09 -0500

    Added next and previous buttons

--- generate_post.py
+++ generate_post.py
@@ -41,12 +41,6 @@ for i in tags_list:
 
 tags_html = tags_html[:-2] # remove the last comma and space
 
-# Finalise the template
-
-final_html = template.replace("{{title}}", title)
-final_html = final_html.replace("{{body}}", text)
-final_html = final_html.replace("{{tags}}", tags_html)
-
 # Make sure the filename of the output is valid for a url (TODO: More checking here)
 
 output_filename = filename.partition(".")[0]
@@ -61,10 +55,34 @@ current_time = datetime.date.today()
 dir_path = Path(f"site/{current_time.year}/{current_time.month}/{current_time.day}")
 dir_path.mkdir(parents=True, exist_ok=True)
 
+# Get the previous post from the previous post file
+
+prev_post_link = ""
+
+try:
+    with open("prev_post_link.txt") as f:
+        prev_post_link = f.read()
+except FileNotFoundError:
+    pass # just leave the string blank
+
+# Finalise the template
+
+final_html = template.replace("{{title}}", title)
+final_html = final_html.replace("{{body}}", text)
+final_html = final_html.replace("{{tags}}", tags_html)
+
+if prev_post_link: # account for first post, where there is no prev_post_link
+    final_html = final_html.replace('<a class="prev">Previous</a>', f'<a class="prev" href="{prev_post_link}">Previous</a>')
+
 # Write the file. Will raise an error if the file already exists
 
 with open(f"site/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html", "x") as f:
     f.write(final_html)
+
+# Set the previous post file to this post
+
+with open("prev_post_link.txt", "w") as f:
+    f.write(f"/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html")
 
 # Add a link to the file on the homepage (index.html)
 
@@ -104,6 +122,24 @@ for i in tags_list:
 
         with open(f"site/tags/{i}.html", "w") as f:
             f.write(tag_page_content)
+
+# Update the "next" link on the previous post to point to this one
+
+prev_post_html = ""
+
+if prev_post_link:
+    try:
+        with open("site" + prev_post_link) as f:
+            prev_post_html = f.read()
+        
+        prev_post_html = prev_post_html.replace('<a class="next">Next</a>', f'<a class="next" href="/{current_time.year}/{current_time.month}/{current_time.day}/{output_filename}.html">Next</a>')
+
+        with open("site" + prev_post_link, "w") as f:
+            f.write(prev_post_html)
+
+    except FileNotFoundError or PermissionError:
+        pass # it's the first post, no previous one to be updated
+
 
 # Report success
 
